37b6cedc658db3a22d35326c81d0e1d40d881f50
DESKTOP-HNCT28L\Brian <BrianJCongdon@gmail.com>
2021-09-17 22:15:54 -0600

    Added 0 crossing algorithm to double check FFT calculation.  Increased baud rate to 115200

--- Collect_Data.py
+++ Collect_Data.py
@@ -1,37 +1,51 @@
+import pandas as pd
 import serial
 import threading
 import datetime
+from matplotlib import pyplot as plt
 
 # from serial.tools import list_ports
 # ports = list(list_ports.comports(include_links=False))
 # for port in ports:
 #     print(port.name)
 
-baud = 9600
+baud = 115200
 run = 1
-fileName = "analog_data.csv"
+fileName = "analog_data_2.csv"
 arduino = serial.Serial('COM3', baudrate=baud)
-
+raw_data = []
+time_list = []
+start = datetime.datetime.now()
+stop = datetime.datetime.now()
 
 
 def normal():
     global run
+    global raw_data
+    global time_list
+    global start
+    global stop
     # Write column headers
-    with open(fileName, "a") as file:
-        c1 = 'Amplitude'
-        c2 = 'Time'
-        file.write(f'{c1},{c2}\n')  # write data with a newline
+    # with open(fileName, "a") as file:
+    #     c1 = 'Amplitude'
+    #     c2 = 'Time'
+    #     file.write(f'{c1},{c2}\n')  # write data with a newline
+
+    start = datetime.datetime.now()
     while run == 1:
-        get_data = arduino.readline()
-        data = int(get_data.strip())
+        raw_data.append(arduino.readline())
+        time_list.append(datetime.datetime.now())
+        # get_data = arduino.readline()
+        # data = int(get_data.strip())
         # print(int(get_data.strip()))
 
         # append the data to the file
-        with open(fileName, "a") as d_file:
-            # write data with a newline
-            d_file.write(f'{data},{datetime.datetime.now()}\n')
+        # with open(fileName, "a") as d_file:
+        #     # write data with a newline
+        #     d_file.write(f'{data},{datetime.datetime.now()}\n')
 
         if not run:
+            stop = datetime.datetime.now()
             print('The while loop is now closing')
 
 
@@ -48,3 +62,11 @@ n = threading.Thread(target=normal)
 i = threading.Thread(target=get_input)
 n.start()
 i.start()
+n.join()
+i.join()
+
+df = pd.DataFrame(raw_data, columns=['Raw'])
+df['Time'] = time_list
+df = df[10:-10]
+df['Amplitude'] = df['Raw'].apply(lambda x: int(x.strip()))
+df.to_csv(fileName, columns=['Amplitude', 'Time'])
--- Process_Data.py
+++ Process_Data.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.fft import fft, fftfreq
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('analog-data.csv')
+data = pd.read_csv('analog_data_2.csv')
 # some_data = data.tail(400)
 # some_data.plot(kind='line',
 #                x='Time',
@@ -11,6 +11,10 @@ data = pd.read_csv('analog-data.csv')
 # plt.show()
 
 data['Normalized'] = np.int16((data['Amplitude'] / data['Amplitude'].max()) * 32767)
+max = data['Amplitude'].max()
+min = data['Amplitude'].min()
+median = int((max-min)/2) + min
+data['Signed'] = data['Amplitude'] - median
 data['Time'] = pd.to_datetime(data['Time'])
 # some_data = data.tail(400)
 # some_data.plot(kind='line',
@@ -19,13 +23,20 @@ data['Time'] = pd.to_datetime(data['Time'])
 # plt.show()
 
 N = len(data)
-mx = max(data['Time'])
-mn = min(data['Time'])
+mx = data['Time'].max()
+mn = data['Time'].min()
 i = mx-mn
+seconds = i.total_seconds()
+zero_crossings = np.where(np.diff(np.sign([i for i in data['Signed'] if i])))[0]
+freq = (len(zero_crossings)/seconds)/2
 avg = i.total_seconds()/(N-1)
-yf = fft(data['Normalized'])
+yf = fft(data['Signed'])
 xf = fftfreq(N, avg)
+data['frequency'] = xf
+data['f_amplitude'] = np.abs(yf)
+data = data.sort_values('f_amplitude', ascending=False)
 
+
+input()
 plt.plot(xf, np.abs(yf))
 plt.show()
-
